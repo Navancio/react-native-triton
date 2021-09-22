c1523965cc5a6ec062f2618f0124b614c95d63d0
Bruce Thwaits <brucethwaits@gmail.com>
2021-09-22 11:32:36 +0200

    Add typescript types

---
+++ index.d.ts
@@ -0,0 +1,21 @@
+declare module 'react-native-triton-player' {
+  //export function configure({brand});
+
+  export function play(tritonName, tritonMount);
+
+  export function playOnDemandStream(trackURL);
+
+  export function pause();
+
+  export function unPause();
+
+  export function stop();
+
+  export function quit();
+
+  export function addStreamChangeListener(callback);
+
+  export function addTrackChangeListener(callback);
+
+  export function addStateChangeListener(callback);
+}
